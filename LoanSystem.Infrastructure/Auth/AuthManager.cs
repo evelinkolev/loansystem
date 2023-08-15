c03c35745b9629536220e63997b4f079c3694371
Evelin Kolev <evelinkolev@vivaldi.net>
2023-08-15 10:55:57 +0300

    install packages for validate, serialize and issue a valid jwt tokens

---
+++ LoanSystem.Infrastructure/Auth/AuthManager.cs
@@ -0,0 +1,25 @@
+﻿using LoanSystem.Application.Abstraction.IAuthManager;
+using LoanSystem.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanSystem.Infrastructure.Auth
+{
+    public partial class AuthManager : IAuthManager
+    {
+        private readonly JwtOptions _jwtOptions;
+
+        public AuthManager(JwtOptions jwtOptions)
+        {
+            _jwtOptions = jwtOptions;
+        }
+
+        public string GenerateToken(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LoanSystem.Infrastructure/Auth/JwtOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace LoanSystem.Infrastructure.Auth
+{
+    public class JwtOptions
+    {
+        public string? Issuer { get; set; }
+        public string? IssuerSigningKey { get; set; }
+        public TimeSpan Expiry { get; set; }
+        public string? Audience { get; set; }
+    }
+}
