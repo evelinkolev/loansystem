cf53b9500c3c5ca00adfd58eed8cb28bb2c267c1
Evelin Kolev <evelinkolev@vivaldi.net>
2023-08-15 09:54:39 +0300

    install and register MediatR (incl. inplementation of logging behavior)

--- LoanSystem.Application/Extensions.cs
+++ LoanSystem.Application/Extensions.cs
@@ -1,8 +1,19 @@
-﻿namespace LoanSystem.Application
+﻿using LoanSystem.Application.Behaviors;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LoanSystem.Application
 {
     public static class Extensions
     {
+        public static IServiceCollection AddApplication(
+            this IServiceCollection services)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(Extensions).Assembly));
+            services.AddSingleton(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
 
+            return services;
+        }
 
     }
 }
