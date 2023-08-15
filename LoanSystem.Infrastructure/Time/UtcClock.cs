1711fd58f34e31c875e0e99cd221555e37910e0c
Evelin Kolev <evelinkolev@vivaldi.net>
2023-08-15 11:06:25 +0300

    current date operation in implementation of UtcClock

---
+++ LoanSystem.Infrastructure/Time/UtcClock.cs
@@ -0,0 +1,12 @@
+﻿using LoanSystem.Application.Abstraction.Time;
+
+namespace LoanSystem.Infrastructure.Time
+{
+    public partial class UtcClock : IClock
+    {
+        public DateTime CurrentDate()
+        {
+            return DateTime.UtcNow;
+        }
+    }
+}
