c0c3d8615ab942a8a331ab8993290983030e0f61
Evelin Kolev <evelinkolev@vivaldi.net>
2023-08-16 08:34:35 +0300

    hash operations in implementation of PasswordHasher

---
+++ LoanSystem.Infrastructure/Pwd/PasswordHasher.cs
@@ -0,0 +1,25 @@
+﻿using LoanSystem.Application.Abstraction.Pwd;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace LoanSystem.Infrastructure.Pwd
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using var hmac = new HMACSHA512();
+            passwordSalt = hmac.Key;
+            passwordHash = hmac
+                .ComputeHash(Encoding.UTF8.GetBytes(password));
+        }
+
+        public bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using var hmac = new HMACSHA512(passwordSalt);
+            var computedHash =
+                hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return computedHash.SequenceEqual(passwordHash);
+        }
+    }
+}
