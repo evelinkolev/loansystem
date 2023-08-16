11cb3ac5f240cd8137a044d7bff2cfd31674ec71
Evelin Kolev <evelinkolev@vivaldi.net>
2023-08-16 08:26:16 +0300

    hash operations in definition of IPasswordHasher

---
+++ LoanSystem.Application/Abstraction/Pwd/IPasswordHasher.cs
@@ -0,0 +1,8 @@
+﻿namespace LoanSystem.Application.Abstraction.Pwd
+{
+    public interface IPasswordHasher
+    {
+        void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt);
+        bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt);
+    }
+}
