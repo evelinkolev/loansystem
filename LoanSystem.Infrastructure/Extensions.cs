fadcf5fddb3935453b4c372c09c48500002371ab
Evelin Kolev <evelinkolev@vivaldi.net>
2023-08-16 08:08:52 +0300

    setting up jwt support

--- LoanSystem.Infrastructure/Extensions.cs
+++ LoanSystem.Infrastructure/Extensions.cs
@@ -1,10 +1,14 @@
 ﻿using LoanSystem.Application.Abstraction.Persistence;
+using LoanSystem.Infrastructure.Auth;
 using LoanSystem.Infrastructure.Persistence;
 using LoanSystem.Infrastructure.Persistence.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace LoanSystem.Infrastructure
 {
@@ -15,7 +19,8 @@ namespace LoanSystem.Infrastructure
             ConfigurationManager configuration)
         {
             services
-                .AddPersistence(configuration);
+                .AddPersistence(configuration)
+                .AddAuth(configuration);
 
             return services;
         }
@@ -29,6 +34,46 @@ namespace LoanSystem.Infrastructure
                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
             services.AddScoped<IUserRepository, UserRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddAuth(
+            this IServiceCollection services,
+            ConfigurationManager configuration)
+        {
+            var jwtOptions = new JwtOptions();
+            configuration.Bind(nameof(jwtOptions), jwtOptions);
+
+            services.AddSingleton(jwtOptions);
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtOptions.IssuerSigningKey!)),
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                RequireExpirationTime = true,
+                ValidateLifetime = true,
+                ValidIssuer = jwtOptions.Issuer,
+                ValidAudience = jwtOptions.Audience,
+            };
+
+            services.AddSingleton(tokenValidationParameters);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = tokenValidationParameters;
+                });
+
+            services.AddAuthorization();
 
             return services;
         }
