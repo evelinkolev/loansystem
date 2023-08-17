73ad77fff7572ca500ac98905b727cb5680a0b97
Evelin Kolev <evelinkolev@vivaldi.net>
2023-08-17 10:07:44 +0300

    setting up registration policy evaluator

---
+++ LoanSystem.Application/RegistrationOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace LoanSystem.Application
+{
+    public class RegistrationOptions
+    {
+        public bool Enabled { get; set; }
+        public IEnumerable<string>? InvalidEmailProviders { get; set; }
+    }
+}
