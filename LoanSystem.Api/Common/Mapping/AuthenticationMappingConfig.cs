2c3f574e6dc497aa5c3ba39fd2c263ae5d0105aa
Evelin Kolev <evelinkolev@vivaldi.net>
2023-08-17 11:15:40 +0300

    install and register Mapster (incl. auth mapping config)

---
+++ LoanSystem.Api/Common/Mapping/AuthenticationMappingConfig.cs
@@ -0,0 +1,15 @@
+﻿using LoanSystem.Application.Auth.Common;
+using LoanSystem.Contracts.V1.Auth.Responses;
+using Mapster;
+
+namespace LoanSystem.Api.Common.Mapping
+{
+    public class AuthenticationMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<AuthenticationResult, AuthenticationResponse>()
+                .Map(dest => dest, src => src.User);
+        }
+    }
+}
