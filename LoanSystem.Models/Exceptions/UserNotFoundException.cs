a0b9320945e833a0bcecf835664e4b846f615327
Evelin Kolev <evelinkolev@vivaldi.net>
2023-08-19 09:26:40 +0300

    create a custom user exception

---
+++ LoanSystem.Models/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿namespace LoanSystem.Models.Exceptions
+{
+    public class UserNotFoundException : Exception
+    {
+        public string? Email { get; }
+        public Guid UserId { get; }
+
+        public UserNotFoundException(Guid userId) : base($"User with ID: '{userId}' was not found.")
+        {
+            UserId = userId;
+        }
+
+        public UserNotFoundException(string email) : base($"User with email: '{email}' was not found.")
+        {
+            Email = email;
+        }
+    }
+}
