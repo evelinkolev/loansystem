5da20a3112125fce7b104cad11fbc4b2563e24eb
Evelin Kolev <evelinkolev@vivaldi.net>
2023-08-20 06:54:43 +0300

    handle sign in

--- LoanSystem.Application/Auth/Queries/Signin/SigninQueryHandler.cs
+++ LoanSystem.Application/Auth/Queries/Signin/SigninQueryHandler.cs
@@ -1,13 +1,38 @@
-﻿using LoanSystem.Application.Auth.Common;
+﻿using LoanSystem.Application.Abstraction.Auth;
+using LoanSystem.Application.Abstraction.Persistence;
+using LoanSystem.Application.Abstraction.Pwd;
+using LoanSystem.Application.Auth.Common;
+using LoanSystem.Models.Exceptions;
 using MediatR;
 
 namespace LoanSystem.Application.Auth.Queries.Signin
 {
     internal sealed class SigninQueryHandler : IRequestHandler<SigninQuery, AuthenticationResult>
     {
-        public Task<AuthenticationResult> Handle(SigninQuery request, CancellationToken cancellationToken)
+        private readonly IUserRepository _userRepository;
+        private readonly IAuthManager _authManager;
+        private readonly IPasswordHasher _passwordHasher;
+
+        public SigninQueryHandler(IUserRepository userRepository, IAuthManager authManager, IPasswordHasher passwordHasher)
         {
-            throw new NotImplementedException();
+            _userRepository = userRepository;
+            _authManager = authManager;
+            _passwordHasher = passwordHasher;
+        }
+        public async Task<AuthenticationResult> Handle(SigninQuery query, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetAsync(query.Email.ToLowerInvariant()) ?? throw new InvalidCredentialsException();
+
+            if (!_passwordHasher.VerifyPasswordHash(query.Password, user.PasswordHash!, user.PasswordSalt!))
+            {
+                throw new InvalidCredentialsException();
+            }
+
+            var token = _authManager.GenerateToken(user);
+
+            return new AuthenticationResult(
+                user,
+                token);
         }
     }
 }
