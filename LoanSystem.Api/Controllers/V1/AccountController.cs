4798afd4230754d974fe654f267fe99225a887f1
Evelin Kolev <evelinkolev@vivaldi.net>
2023-08-20 07:13:47 +0300

    user sign up and controller account

---
+++ LoanSystem.Api/Controllers/V1/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using LoanSystem.Application.Auth.Commands.Signup;
+using LoanSystem.Contracts.V1;
+using LoanSystem.Contracts.V1.Auth.Requests;
+using LoanSystem.Contracts.V1.Auth.Responses;
+using MapsterMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoanSystem.Api.Controllers.V1
+{
+    [AllowAnonymous]
+    public class AccountController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public AccountController(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        [HttpPost(ApiRoutes.Account.Signup)]
+        public async Task<ActionResult> SignupAsync([FromBody] SignupRequest request)
+        {
+            var command = _mapper.Map<SignupCommand>(request);
+            var result = await _mediator.Send(command);
+            return Ok(_mapper.Map<AuthenticationResponse>(result));
+        }
+    }
+}
