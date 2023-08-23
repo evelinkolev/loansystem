398eb5dbff15771ca0665588af1856152fcdf708
Evelin Kolev <evelinkolev@vivaldi.net>
2023-08-23 08:35:37 +0300

    create payer operation in implementation of PayerRepository

---
+++ LoanSystem.Infrastructure/Persistence/Repositories/PayerRepository.cs
@@ -0,0 +1,23 @@
+﻿using LoanSystem.Application.Abstraction.Persistence;
+using LoanSystem.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoanSystem.Infrastructure.Persistence.Repositories
+{
+    internal sealed class PayerRepository : IPayerRepository
+    {
+        private readonly LoanSystemContext _context;
+        private readonly DbSet<Payer> _payers;
+
+        public PayerRepository(LoanSystemContext context)
+        {
+            _context = context;
+            _payers = _context.Payers;
+        }
+        public async Task CreateAsync(Payer payer)
+        {
+            await _payers.AddAsync(payer);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
