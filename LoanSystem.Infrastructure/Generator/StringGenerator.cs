a4ddd40b0b555c586ff62063d2f639f23159f084
Evelin Kolev <evelinkolev@vivaldi.net>
2023-08-23 08:52:01 +0300

    generate routing number operation in implementation of StringGenerator

---
+++ LoanSystem.Infrastructure/Generator/StringGenerator.cs
@@ -0,0 +1,25 @@
+﻿using LoanSystem.Application.Abstraction.Generator;
+using System.Text;
+
+namespace LoanSystem.Infrastructure.Generator
+{
+    public partial class StringGenerator : IStringGenerator
+    {
+        private readonly Random _random;
+
+        public StringGenerator()
+        {
+            _random = new Random();
+        }
+        public string Generate8DigitRoutingNumber()
+        {
+            var stringBuilder = new StringBuilder();
+            while (stringBuilder.Length < 8)
+            {
+                stringBuilder.Append(_random.Next(10).ToString());
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
