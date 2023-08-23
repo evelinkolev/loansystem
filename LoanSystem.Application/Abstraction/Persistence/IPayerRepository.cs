24eaf6d5b99ffcc3568ca0fd8c59a553deaca92c
Evelin Kolev <evelinkolev@vivaldi.net>
2023-08-23 08:31:19 +0300

    create payer operation in definition of IPayerRepository

---
+++ LoanSystem.Application/Abstraction/Persistence/IPayerRepository.cs
@@ -0,0 +1,9 @@
+﻿using LoanSystem.Models.Domain;
+
+namespace LoanSystem.Application.Abstraction.Persistence
+{
+    public interface IPayerRepository
+    {
+        Task CreateAsync(Payer payer);
+    }
+}
