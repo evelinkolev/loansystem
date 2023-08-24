abcfe0af38081d7010789171f1ddb1c2de3d98f5
Evelin Kolev <evelinkolev@vivaldi.net>
2023-08-24 07:42:17 +0300

    create payer exception

---
+++ LoanSystem.Models/Exceptions/PayerArgumentException.cs
@@ -0,0 +1,18 @@
+﻿namespace LoanSystem.Models.Exceptions
+{
+    public class PayerArgumentException : Exception
+    {
+        public string? FullName { get; }
+        public decimal Deposit { get; }
+
+        public PayerArgumentException(string fullName) : base($"Payer with Full Name: '{fullName}' is invalid.")
+        {
+            FullName = fullName;
+        }
+
+        public PayerArgumentException(decimal deposit): base($"Payer with Deposit: '{deposit}' is invalid.")
+        {
+            Deposit = deposit;
+        }
+    }
+}
