3df5f48c7426d24408b1c3da45e6c45cc9639535
Evelin Kolev <evelinkolev@vivaldi.net>
2023-08-25 15:23:33 +0300

    handle deposit money into private user-payer account

---
+++ LoanSystem.Application/Payers/Commands/SetupPayerDirectDeposit/SetupPayerDirectDepositCommand.cs
@@ -0,0 +1,11 @@
+﻿using LoanSystem.Models.Domain;
+using MediatR;
+
+namespace LoanSystem.Application.Payers.Commands.SetupPayerDirectDeposit
+{
+    public record SetupPayerDirectDepositCommand(
+        Guid PayerId,
+        decimal Deposit,
+        string RoutingNumber,
+        string AccountNumber) : IRequest<Payer>;
+}
---
+++ LoanSystem.Application/Payers/Commands/SetupPayerDirectDeposit/SetupPayerDirectDepositCommandHandler.cs
@@ -0,0 +1,65 @@
+﻿using LoanSystem.Application.Abstraction.Identity;
+using LoanSystem.Application.Abstraction.Persistence;
+using LoanSystem.Application.Abstraction.Time;
+using LoanSystem.Models.Domain;
+using LoanSystem.Models.Exceptions;
+using MediatR;
+using System.Security.Claims;
+
+namespace LoanSystem.Application.Payers.Commands.SetupPayerDirectDeposit
+{
+    internal sealed class SetupPayerDirectDepositCommandHandler :
+        IRequestHandler<SetupPayerDirectDepositCommand, Payer>
+    {
+        private readonly IPayerRepository _payerRepository;
+        private readonly IUserAccessor _userAccessor;
+        private readonly IClock _clock;
+
+        public SetupPayerDirectDepositCommandHandler(IPayerRepository payerRepository, IUserAccessor userAccessor, IClock clock)
+        {
+            _payerRepository = payerRepository;
+            _userAccessor = userAccessor;
+            _clock = clock;
+        }
+
+        public async Task<Payer> Handle(SetupPayerDirectDepositCommand command, CancellationToken cancellationToken)
+        {
+            var payer = await _payerRepository.GetAsync(command.PayerId) ?? throw new PayerNotFoundException(command.PayerId);
+
+            var userId = _userAccessor.User.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+
+            var isValid = await _payerRepository.UserHavePayerAsync(payer.Id, Guid.Parse(userId));
+
+            if (!isValid)
+            {
+                throw new BadRequestPayerException();
+            }
+
+            if (command.RoutingNumber != payer.RoutingNumber)
+            {
+                throw new InvalidRoutingNumberException(command.RoutingNumber);
+            }
+
+            if (command.AccountNumber != payer.AccountNumber)
+            {
+                throw new InvalidAccountNumberException(command.AccountNumber);
+            }
+
+            decimal sum = payer.Deposit + command.Deposit;
+
+            if (sum > decimal.MaxValue || command.Deposit < 0)
+            {
+                throw new PayerArgumentException(command.Deposit);
+            }
+
+            var now = _clock.CurrentDate();
+
+            payer.Deposit += command.Deposit;
+            payer.UpdatedDateTime = now;
+
+            await _payerRepository.SetUpPayerDirectDepositAsync(payer);
+
+            return payer;
+        }
+    }
+}
