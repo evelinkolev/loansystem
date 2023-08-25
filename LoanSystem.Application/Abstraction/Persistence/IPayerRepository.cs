fe5a1e2062183ebb5636d73fe5a235c289fc8715
Evelin Kolev <evelinkolev@vivaldi.net>
2023-08-25 14:24:45 +0300

    claim tracking operation in definition of IPayerRepository

--- LoanSystem.Application/Abstraction/Persistence/IPayerRepository.cs
+++ LoanSystem.Application/Abstraction/Persistence/IPayerRepository.cs
@@ -7,5 +7,6 @@ namespace LoanSystem.Application.Abstraction.Persistence
         Task CreateAsync(Payer payer);
         Task<Payer?> GetAsync(Guid id);
         Task SetUpPayerDirectDepositAsync(Payer payer);
+        Task<bool> UserHavePayerAsync(Guid payerId, Guid userId);
     }
 }
