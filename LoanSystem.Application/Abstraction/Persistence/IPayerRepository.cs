0a846c9482781d929d336b62171b935813f11852
Evelin Kolev <evelinkolev@vivaldi.net>
2023-08-25 14:17:29 +0300

    set up payer direct deposit operation in definition of IPayerRepository

--- LoanSystem.Application/Abstraction/Persistence/IPayerRepository.cs
+++ LoanSystem.Application/Abstraction/Persistence/IPayerRepository.cs
@@ -6,5 +6,6 @@ namespace LoanSystem.Application.Abstraction.Persistence
     {
         Task CreateAsync(Payer payer);
         Task<Payer?> GetAsync(Guid id);
+        Task SetUpPayerDirectDepositAsync(Payer payer);
     }
 }
