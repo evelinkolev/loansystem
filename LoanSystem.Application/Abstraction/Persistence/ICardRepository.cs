ecaa8d003f52cd4208f7c8503d406574ab059dc2
Evelin Kolev <evelinkolev@vivaldi.net>
2023-08-28 10:42:07 +0300

    create card operation in definition of ICardRepository

---
+++ LoanSystem.Application/Abstraction/Persistence/ICardRepository.cs
@@ -0,0 +1,9 @@
+﻿using LoanSystem.Models.Domain;
+
+namespace LoanSystem.Application.Abstraction.Persistence
+{
+    public interface ICardRepository
+    {
+        Task CreateAsync(Card card);
+    }
+}
