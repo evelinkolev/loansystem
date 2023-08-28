abf8fab2f099d4809dca043977116a594c17a755
Evelin Kolev <evelinkolev@vivaldi.net>
2023-08-28 10:46:39 +0300

    create card operation in implementation of CardRepository

---
+++ LoanSystem.Infrastructure/Persistence/Repositories/CardRepository.cs
@@ -0,0 +1,24 @@
+﻿using LoanSystem.Application.Abstraction.Persistence;
+using LoanSystem.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoanSystem.Infrastructure.Persistence.Repositories
+{
+    internal sealed class CardRepository : ICardRepository
+    {
+        private readonly LoanSystemContext _context;
+        private readonly DbSet<Card> _cards;
+
+        public CardRepository(LoanSystemContext context)
+        {
+            _context = context;
+            _cards = _context.Cards;
+        }
+
+        public async Task CreateAsync(Card card)
+        {
+            await _cards.AddAsync(card);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
