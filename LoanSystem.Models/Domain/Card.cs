b9b0b7f915179de75b7f9cd1a50a76c7f0ccf6d1
Evelin Kolev <evelinkolev@vivaldi.net>
2023-08-28 10:18:24 +0300

    create a Card object for a Payer

---
+++ LoanSystem.Models/Domain/Card.cs
@@ -0,0 +1,14 @@
+﻿using LoanSystem.Models.Domain.Base;
+
+namespace LoanSystem.Models.Domain
+{
+    public partial class Card : EntityBase
+    {
+        public string? Number { get; set; }
+        public string? HolderName { get; set; }
+        public string? ExpiryDate { get; set; }
+        public string? SecurityCode { get; set; }
+        public Guid PayerId { get; set; }
+        public Payer? Payer { get; set; }
+    }
+}
