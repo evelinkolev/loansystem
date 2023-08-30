5a190f68a8e0568b9cb19330aacfa985028dde26
Evelin Kolev <evelinkolev@vivaldi.net>
2023-08-30 09:23:26 +0300

    generate card number operation in implementation of StringGenerator

--- LoanSystem.Infrastructure/Generator/StringGenerator.cs
+++ LoanSystem.Infrastructure/Generator/StringGenerator.cs
@@ -29,6 +29,48 @@ namespace LoanSystem.Infrastructure.Generator
             return checkSum % 10 == 0;
         }
 
+        public string Generate16DigitNumber()
+        {
+            var stringBuilder = new StringBuilder();
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            var first3CardDigits = new string(
+                                Enumerable.Repeat(chars, 3)
+                                .Select(s => s[_random.Next(s.Length)])
+                                .ToArray());
+
+            stringBuilder.Append(first3CardDigits);
+
+            while (stringBuilder.Length < 12)
+            {
+                stringBuilder.Append(_random.Next(0, 10));
+            }
+
+            stringBuilder.Append(CreateCheckDigit(stringBuilder.ToString()));
+
+            return !CheckLuhn(stringBuilder.ToString()) ? null! : stringBuilder.ToString();
+        }
+
+        private string CreateCheckDigit(string number)
+        {
+            var digitsSum = number
+                .ToCharArray()
+                .Where(c => !char.IsWhiteSpace(c))
+                .ToArray()
+                .Reverse()
+                .Select(CharToInt)
+                .Select((digit, index) => isEven(index) ? doubleDigit(digit) : digit)
+                .Sum();
+
+            digitsSum *= 9;
+
+            return digitsSum
+                .ToString()
+                .ToCharArray()
+                .Last()
+                .ToString();
+        }
+
         public string Generate3DigitSecurityCode()
         {
             var stringBuilder = new StringBuilder();
