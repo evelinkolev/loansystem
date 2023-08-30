7f47d55c7f941241689836e1cf0de62d6ee6ef01
Evelin Kolev <evelinkolev@vivaldi.net>
2023-08-30 09:06:18 +0300

    generate card number operation in definition of IStringGenerator

--- LoanSystem.Application/Abstraction/Generator/IStringGenerator.cs
+++ LoanSystem.Application/Abstraction/Generator/IStringGenerator.cs
@@ -5,6 +5,7 @@
         string Generate8DigitRoutingNumber();
         string Generate9DigitAccountNumber();
         string Generate3DigitSecurityCode();
+        string Generate16DigitNumber();
         bool CheckLuhn(string creditCardNumber);
     }
 }
