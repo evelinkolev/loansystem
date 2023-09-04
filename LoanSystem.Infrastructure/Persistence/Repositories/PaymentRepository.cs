549651ad3f442158626c4535ac0e00008034b5a0
Evelin Kolev <evelinkolev@vivaldi.net>
2023-09-04 07:42:38 +0300

    create payment operation in implementation of PaymentRepository

---
+++ LoanSystem.Infrastructure/Persistence/Repositories/PaymentRepository.cs
@@ -0,0 +1,24 @@
+﻿using LoanSystem.Application.Abstraction.Persistence;
+using LoanSystem.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoanSystem.Infrastructure.Persistence.Repositories
+{
+    internal sealed class PaymentRepository : IPaymentRepository
+    {
+        private readonly LoanSystemContext _context;
+        private readonly DbSet<Payment> _payments;
+
+        public PaymentRepository(LoanSystemContext context)
+        {
+            _context = context;
+            _payments = _context.Payments;
+        }
+
+        public async Task CreateAsync(Payment payment)
+        {
+            await _payments.AddAsync(payment);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
