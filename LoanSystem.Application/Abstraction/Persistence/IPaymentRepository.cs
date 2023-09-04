95e0e8df06521d393a782ab3f59ded0aa9dc44e3
Evelin Kolev <evelinkolev@vivaldi.net>
2023-09-04 07:27:39 +0300

    create payment operation in definition of IPaymentRepository

---
+++ LoanSystem.Application/Abstraction/Persistence/IPaymentRepository.cs
@@ -0,0 +1,9 @@
+﻿using LoanSystem.Models.Domain;
+
+namespace LoanSystem.Application.Abstraction.Persistence
+{
+    public interface IPaymentRepository
+    {
+        Task CreateAsync(Payment payment);
+    }
+}
