a6a33c0ada357399b8f84bc325a1e415b402236f
Evelin Kolev <evelinkolev@vivaldi.net>
2023-09-04 07:00:43 +0300

    create a Payment object for a Payer

---
+++ LoanSystem.Models/Domain/Payment.cs
@@ -0,0 +1,12 @@
+﻿using LoanSystem.Models.Domain.Base;
+
+namespace LoanSystem.Models.Domain
+{
+    public partial class Payment : EntityBase
+    {
+        public decimal Amount { get; set; }
+        public string RequestType { get; set; } = "ONE_TIME";
+        public Guid PayerId { get; set; }
+        public Payer? Payer { get; set; }
+    }
+}
