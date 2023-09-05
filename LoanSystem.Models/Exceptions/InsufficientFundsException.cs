515b83391219e95d2081be865188acc5f0907496
Evelin Kolev <evelinkolev@vivaldi.net>
2023-09-05 08:10:34 +0300

    create payment exception

---
+++ LoanSystem.Models/Exceptions/InsufficientFundsException.cs
@@ -0,0 +1,10 @@
+﻿namespace LoanSystem.Models.Exceptions
+{
+    public class InsufficientFundsException : Exception
+    {
+        public InsufficientFundsException() : base($"Transaction draws money from a bank account while the account balance is lower than the amount drawn.")
+        {
+            
+        }
+    }
+}
