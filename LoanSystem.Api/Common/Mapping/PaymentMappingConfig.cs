3d022c3ca94ec2453b3e4f7705ee054965ebe3a2
Evelin Kolev <evelinkolev@vivaldi.net>
2023-09-05 08:38:11 +0300

    configure payment mapping

---
+++ LoanSystem.Api/Common/Mapping/PaymentMappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using LoanSystem.Contracts.V1.Payments.Responses;
+using LoanSystem.Models.Domain;
+using Mapster;
+
+namespace LoanSystem.Api.Common.Mapping
+{
+    public class PaymentMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<Payment, PaymentResponse>()
+                .Map(dest => dest.Id, src => src.Id)
+                .Map(dest => dest.Amount, src => src.Amount)
+                .Map(dest => dest.RequestType, src => src.RequestType)
+                .Map(dest => dest.CreatedDateTime, src => src.CreatedDateTime)
+                .Map(dest => dest.UpdatedDateTime, src => src.UpdatedDateTime)
+                .Map(dest => dest.PayerId, src => src.PayerId);
+        }
+    }
+}
