7ab9f9bc3da558f4cecc48c9cc085483273a2874
Evelin Kolev <evelinkolev@vivaldi.net>
2023-09-05 08:46:20 +0300

    use http post to create a resource

---
+++ LoanSystem.Api/Controllers/V1/PaymentsController.cs
@@ -0,0 +1,34 @@
+﻿using LoanSystem.Application.Payments.Commands.CreatePayment;
+using LoanSystem.Contracts.V1;
+using LoanSystem.Contracts.V1.Payments.Requests;
+using LoanSystem.Contracts.V1.Payments.Responses;
+using MapsterMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoanSystem.Api.Controllers.V1
+{
+    [Authorize]
+    public class PaymentsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public PaymentsController(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        [HttpPost(ApiRoutes.Payments.Create)]
+        public async Task<ActionResult> CreateAsync([FromBody] CreatePaymentRequest request)
+        {
+            var command = _mapper.Map<CreatePaymentCommand>(request);
+
+            var result = await _mediator.Send(command);
+
+            return Ok(_mapper.Map<PaymentResponse>(result));
+        }
+    }
+}
