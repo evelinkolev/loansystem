013debae684e999edfb8f5060028e4c922c172cc
Evelin Kolev <evelinkolev@vivaldi.net>
2023-09-09 07:09:47 +0300

    handle get payer by id query

---
+++ LoanSystem.Application/Payers/Queries/GetPayerById/GetPayerByIdQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using LoanSystem.Application.Abstraction.Persistence;
+using LoanSystem.Models.Domain;
+using LoanSystem.Models.Exceptions;
+using MediatR;
+
+namespace LoanSystem.Application.Payers.Queries.GetPayerById
+{
+    internal sealed class GetPayerByIdQueryHandler : IRequestHandler<GetPayerByIdQuery, Payer>
+    {
+        private readonly IPayerRepository _payerRepository;
+
+        public GetPayerByIdQueryHandler(IPayerRepository payerRepository)
+        {
+            _payerRepository = payerRepository;
+        }
+
+        public async Task<Payer> Handle(GetPayerByIdQuery query, CancellationToken cancellationToken)
+        {
+            return await _payerRepository.GetAsync(query.Id) ?? throw new PayerNotFoundException(query.Id);
+        }
+    }
+}
