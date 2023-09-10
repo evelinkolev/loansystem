a1a72161489733d1c6dd0a31293e61dad614d330
Evelin Kolev <evelinkolev@vivaldi.net>
2023-09-10 08:15:18 +0300

    handle get card by id query

---
+++ LoanSystem.Application/Cards/Queries/GetCard/GetCardQuery.cs
@@ -0,0 +1,7 @@
+﻿using LoanSystem.Models.Domain;
+using MediatR;
+
+namespace LoanSystem.Application.Cards.Queries.GetCard
+{
+    public record GetCardQuery(Guid Id) : IRequest<Card>;
+}
---
+++ LoanSystem.Application/Cards/Queries/GetCard/GetCardQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using LoanSystem.Application.Abstraction.Persistence;
+using LoanSystem.Models.Domain;
+using LoanSystem.Models.Exceptions;
+using MediatR;
+
+namespace LoanSystem.Application.Cards.Queries.GetCard
+{
+    internal sealed class GetCardQueryHandler : IRequestHandler<GetCardQuery, Card>
+    {
+        private readonly ICardRepository _cardRepository;
+
+        public GetCardQueryHandler(ICardRepository cardRepository)
+        {
+            _cardRepository = cardRepository;
+        }
+
+        public async Task<Card> Handle(GetCardQuery query, CancellationToken cancellationToken)
+        {
+            return await _cardRepository.GetAsync(query.Id) ?? throw new CardNotFoundException(query.Id);
+        }
+    }
+}
