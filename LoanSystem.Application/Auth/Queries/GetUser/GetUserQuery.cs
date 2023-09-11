b4ed3bfb59d4b2f44fc5d3f7fe13201ba6facfae
Evelin Kolev <evelinkolev@vivaldi.net>
2023-09-11 11:21:28 +0300

    handle get user by id query

---
+++ LoanSystem.Application/Auth/Queries/GetUser/GetUserQuery.cs
@@ -0,0 +1,7 @@
+﻿using LoanSystem.Models.Domain;
+using MediatR;
+
+namespace LoanSystem.Application.Auth.Queries.GetUser
+{
+    public record GetUserQuery(Guid Id) : IRequest<User>;
+}
---
+++ LoanSystem.Application/Auth/Queries/GetUser/GetUserQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using LoanSystem.Application.Abstraction.Identity;
+using LoanSystem.Application.Abstraction.Persistence;
+using LoanSystem.Models.Domain;
+using MediatR;
+using System.Security.Claims;
+
+namespace LoanSystem.Application.Auth.Queries.GetUser
+{
+    internal sealed class GetUserQueryHandler : IRequestHandler<GetUserQuery, User>
+    {
+        private readonly IUserAccessor _userAccessor;
+        private readonly IUserRepository _userRepository;
+
+        public GetUserQueryHandler(IUserAccessor userAccessor, IUserRepository userRepository)
+        {
+            _userAccessor = userAccessor;
+            _userRepository = userRepository;
+        }
+
+        public async Task<User> Handle(GetUserQuery query, CancellationToken cancellationToken)
+        {
+            // User cannot access the other user accounts.
+
+            var userId = _userAccessor.User.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+
+            var user = await _userRepository.GetAsync(Guid.Parse(userId));
+
+            return user!;
+        }
+    }
+}
