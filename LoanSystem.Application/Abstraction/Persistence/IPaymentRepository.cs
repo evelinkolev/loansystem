974e5a88c327612667853910177a47ecedec7046
Evelin Kolev <evelinkolev@vivaldi.net>
2023-09-13 13:14:33 +0300

    browse payments operation in definition of IPaymentRepository

--- LoanSystem.Application/Abstraction/Persistence/IPaymentRepository.cs
+++ LoanSystem.Application/Abstraction/Persistence/IPaymentRepository.cs
@@ -4,6 +4,7 @@ namespace LoanSystem.Application.Abstraction.Persistence
 {
     public interface IPaymentRepository
     {
+        Task<IQueryable<Payment>> FindAllAsync();
         Task CreateAsync(Payment payment);
         Task<Payment?> GetAsync(Guid Id);
     }
