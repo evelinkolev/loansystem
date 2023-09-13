c887a8629801213949c83a3bf5fafbac81b2a15b
Evelin Kolev <evelinkolev@vivaldi.net>
2023-09-13 13:26:20 +0300

    support LINQ Contains with value type

--- LoanSystem.Application/Payments/Queries/BrowsePayments/BrowsePaymentsQueryHandler.cs
+++ LoanSystem.Application/Payments/Queries/BrowsePayments/BrowsePaymentsQueryHandler.cs
@@ -1,14 +1,29 @@
-﻿using LoanSystem.Models.Domain;
+﻿using LoanSystem.Application.Abstraction.Persistence;
+using LoanSystem.Models.Domain;
 using MediatR;
 
 namespace LoanSystem.Application.Payments.Queries.BrowsePayments
 {
     internal sealed class BrowsePaymentsQueryHandler : IRequestHandler<BrowsePaymentsQuery, List<Payment>>
     {
-        // To do: make use of dependency injection.
-        public Task<List<Payment>> Handle(BrowsePaymentsQuery query, CancellationToken cancellationToken)
+        private readonly IPaymentRepository _paymentRepository;
+
+        public BrowsePaymentsQueryHandler(IPaymentRepository paymentRepository)
         {
-            throw new NotImplementedException();
+            _paymentRepository = paymentRepository;
+        }
+
+        public async Task<List<Payment>> Handle(BrowsePaymentsQuery query, CancellationToken cancellationToken)
+        {
+            var paymentsQuery = await _paymentRepository.FindAllAsync();
+
+            if (!string.IsNullOrWhiteSpace(query.SearchTerm))
+            {
+                paymentsQuery = paymentsQuery.Where(x => x.Amount.ToString().Contains(query.SearchTerm)
+                    || x.RequestType.Contains(query.SearchTerm));
+            }
+
+            return paymentsQuery.ToList();
         }
     }
 }
