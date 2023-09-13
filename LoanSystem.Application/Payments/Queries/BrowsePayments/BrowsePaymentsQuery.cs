e1f496ef8c49682f569977f59b2eb3a17e489cb3
Evelin Kolev <evelinkolev@vivaldi.net>
2023-09-13 13:08:12 +0300

    define browse payments query

---
+++ LoanSystem.Application/Payments/Queries/BrowsePayments/BrowsePaymentsQuery.cs
@@ -0,0 +1,7 @@
+﻿using LoanSystem.Models.Domain;
+using MediatR;
+
+namespace LoanSystem.Application.Payments.Queries.BrowsePayments
+{
+    public record BrowsePaymentsQuery(string? SearchTerm) : IRequest<List<Payment>>;
+}
---
+++ LoanSystem.Application/Payments/Queries/BrowsePayments/BrowsePaymentsQueryHandler.cs
@@ -0,0 +1,14 @@
+﻿using LoanSystem.Models.Domain;
+using MediatR;
+
+namespace LoanSystem.Application.Payments.Queries.BrowsePayments
+{
+    internal sealed class BrowsePaymentsQueryHandler : IRequestHandler<BrowsePaymentsQuery, List<Payment>>
+    {
+        // To do: make use of dependency injection.
+        public Task<List<Payment>> Handle(BrowsePaymentsQuery query, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
