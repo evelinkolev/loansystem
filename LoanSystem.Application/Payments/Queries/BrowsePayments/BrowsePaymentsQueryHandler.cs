2778422f66efcae75dcc258bcf5e7289f86c7d52
Evelin Kolev <evelinkolev@vivaldi.net>
2023-09-14 07:46:20 +0300

    add support for sorting

--- LoanSystem.Application/Payments/Queries/BrowsePayments/BrowsePaymentsQueryHandler.cs
+++ LoanSystem.Application/Payments/Queries/BrowsePayments/BrowsePaymentsQueryHandler.cs
@@ -1,6 +1,7 @@
 ﻿using LoanSystem.Application.Abstraction.Persistence;
 using LoanSystem.Models.Domain;
 using MediatR;
+using System.Linq.Expressions;
 
 namespace LoanSystem.Application.Payments.Queries.BrowsePayments
 {
@@ -17,13 +18,32 @@ namespace LoanSystem.Application.Payments.Queries.BrowsePayments
         {
             var paymentsQuery = await _paymentRepository.FindAllAsync();
 
+            // Filtering
             if (!string.IsNullOrWhiteSpace(query.SearchTerm))
             {
                 paymentsQuery = paymentsQuery.Where(x => x.Amount.ToString().Contains(query.SearchTerm)
                     || x.RequestType.Contains(query.SearchTerm));
             }
 
+            // Sorting
+            if (query.SortOrder?.ToLower() == "desc")
+            {
+                paymentsQuery = paymentsQuery.OrderByDescending(GetSortProperty(query));
+            }
+            else
+            {
+                paymentsQuery = paymentsQuery.OrderBy(GetSortProperty(query));
+            }
+
             return paymentsQuery.ToList();
         }
+        private static Expression<Func<Payment, object>> GetSortProperty(BrowsePaymentsQuery query) =>
+            query.SortColumn?.ToLower() switch
+            {
+                "date" => payment => payment.CreatedDateTime,
+                "amount" => payment => payment.Amount,
+                "type" => payment => payment.RequestType,
+                _ => payment => payment.Id
+            };
     }
 }
